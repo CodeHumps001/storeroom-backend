14a3d61a735814907cbaf7d6def6706c1e9f6e59
Yaw Fosu <codehumps00233@gmail.com>
2026-05-25 00:16:38 +0000

    pos page

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,11 +1,20 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  // Add these for cloud deployment
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export default transporter;
